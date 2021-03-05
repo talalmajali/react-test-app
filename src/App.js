13351ea0998b43e50354844374d90581895969cf
talalmajali <t.majali93@gmail.com>
2021-03-05 18:31:03 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import OurForm from './forms/OurForm';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container className="p-3">
+    <Jumbotron>
+      <h1 className="header">Welcome To React-Bootstrap</h1>
+    </Jumbotron>
+    </Container>
+    <OurForm />
     </div>
   );
 }
